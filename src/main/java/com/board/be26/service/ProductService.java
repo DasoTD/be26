c10b33c8a907ed6a1664c0ac4b5e378bcebb9215
Timmy <daso.timileyin@gmail.com>
2026-01-03 22:07:49 +0000

    docker

---
+++ src/main/java/com/board/be26/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.board.be26.service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProductService {
+    private static final Logger logger = LoggerFactory.getLogger(ProductService.class);
+    
+}
